0423a236645c1983192409ef5db64e84d2d6da7b
Olyamanevskaya <156808883+Olyamanevskaya@users.noreply.github.com>
2024-05-22 15:21:20 +0700

    some message

--- projects/loft-photo-lite-3/model.js
+++ projects/loft-photo-lite-3/model.js
@@ -1,19 +1,100 @@
 export default {
-  getRandomElement(array) {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
 
-  async getNextPhoto() {},
+    const index = Math.round(Math.random() * (array.length - 1));
 
-  login() {},
+    return array[index];
+  },
 
-  init() {},
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
+
+    return { friend, id: photo.id, url: size.url };
+  },
+
+  friendSize(photo) {
+    const size = photo.sized.find((size) => size.width >= 360);
+
+    if (!size) {
+      return photo.sizes.reduce((biggest, current) => {
+        if (current.width > biggest.width) {
+          return current;
+        }
+
+        return biggest;
+      }, photo.sizes[0]);
+    }
+
+    return size;
+  },
+
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFfriends();
+  },
+
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId: APP_ID,
+      });
+
+      VK.Auth.login((response) => {
+        if (response.session) {
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+  callApi(method, params) {
+    params.v = params.v || '5.120';
+
+    return new Promise((resolve, reject) => {
+      VK.api(method, params, (response) => {
+        if (response.error) {
+          reject(new Error(response.error.error_msg));
+        } else {
+          resolve(response.response);
+        }
+      });
+    });
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    };
+
+    return this.callApi('photos.getAll', params);
+  },
 
   photoCache: {},
-  getFriendPhotos(id) {
-    const photos = this.photoCache[id];
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
 
     if (photos) {
       return photos;
     }
+
+    photos = await this.getPhotos(id);
 
     // const photos = вместо этого комментария вставьте код для получения фотографии друга из ВК
 
