584d80968ab144de5edab8052c363c0354fb9ebb
Olyamanevskaya <156808883+Olyamanevskaya@users.noreply.github.com>
2024-05-27 16:47:54 +0700

    some message

--- projects/loft-photo-lite-4/model.js
+++ projects/loft-photo-lite-4/model.js
@@ -3,19 +3,92 @@ const PERM_PHOTOS = 4;
 const APP_ID = 5350105;
 
 export default {
-  getRandomElement(array) {},
+  getRandomElement(array) {
+    if (!array.length) {
+      return null;
+    }
 
-  async getNextPhoto() {},
+    const index = Math.round(Math.random() * (array.length - 1));
 
-  async init() {},
+    return array[index];
+  },
 
-  login() {},
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.item);
+    const photos = await this.getFriendPhotos(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
 
-  logout() {},
+    return { friend, id: photo.id, url: size.url };
+  },
 
-  getFriends() {},
+  async init() {
+    this.photoCache = {};
+    this.friends = await this.getFfriends();
+    [this.me] = await this.getUsers();
+  },
 
-  getUsers(ids) {},
+  login() {
+    return new Promise((resolve, reject) => {
+      VK.init({
+        apiId: APP_ID,
+      });
 
-  async getFriendPhotos(id) {},
+      VK.Auth.login((response) => {
+        if (response.session) {
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+
+  logout() {
+    return new Promise((resolve) => VK.Auth.revokeGrants(resolve));
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    };
+
+    return this.callApi('photos.getAll', params);
+  }
+
+  getUsers(ids) {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    if (ids) {
+      params.user_ids = ids;
+    }
+
+    return this.callApi('users.get', params);
+  },
+
+  async getFriendPhotos(id) {
+    let photos = this.photoCache[id];
+
+    if (photos) {
+      return photos;
+    }
+
+    photos = await this.getPhotos(id);
+
+    this.photoCache[id] = photos;
+
+    return photos;
+  },
 };
